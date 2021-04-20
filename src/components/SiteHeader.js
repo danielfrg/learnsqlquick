c768e75dcd4803122fd32bf0a9bbeb80e05fe70c
Daniel Rodriguez <daniel@danielfrg.com>
2021-04-20 18:51:25 -0500

    Remove container on hedaer

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -8,25 +8,23 @@ export const HEADER_HEIGHT = 62
 const SiteHeader = ({ siteTitle }) => (
     <Sticky className="site-header">
         <div>
-            <div className="container-xl">
-                <Header className="nav">
-                    <Header.Item>
-                        <Header.Link as={GatsbyLink} to="/" fontSize={3}>
-                            <span>{siteTitle}</span>
-                        </Header.Link>
-                    </Header.Item>
-                    <Header.Item>
-                        <Header.Link as={GatsbyLink} to="/lessons">
-                            Lessons
-                        </Header.Link>
-                    </Header.Item>
-                    <Header.Item>
-                        <Header.Link as={GatsbyLink} to="/questions">
-                            Questions
-                        </Header.Link>
-                    </Header.Item>
-                </Header>
-            </div>
+            <Header className="nav">
+                <Header.Item>
+                    <Header.Link as={GatsbyLink} to="/" fontSize={3}>
+                        <span>{siteTitle}</span>
+                    </Header.Link>
+                </Header.Item>
+                <Header.Item>
+                    <Header.Link as={GatsbyLink} to="/lessons">
+                        Lessons
+                    </Header.Link>
+                </Header.Item>
+                <Header.Item>
+                    <Header.Link as={GatsbyLink} to="/questions">
+                        Questions
+                    </Header.Link>
+                </Header.Item>
+            </Header>
         </div>
     </Sticky>
 )
